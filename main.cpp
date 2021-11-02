86fb14382c3df631b69507dfcf7ea898f13ebc22
Stepan <stepan151@gmail.com>
2021-11-02 14:30:30 +0300

    shit

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include "./ft_vector/ft_vector.hpp"
 
 int main() {
-	std::vector<int> std_vec;
-	std_vec.push_back(1);
-	std_vec.push_back(41);
-	std_vec.push_back(-71);
-	std_vec.push_back(8);
-	ft::vector<int> ft_vec(std_vec.begin(), std_vec.end());
+	std::vector<int> std_vector;
+	std_vector.push_back(1);
+	std_vector.push_back(88);
+	std_vector.push_back(71);
+	std_vector.push_back(81);
+	//ft::vector<int> ft_vec(std_vector.begin(), std_vector.end());
 }
