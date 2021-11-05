0c26437108b3475d13dd7af0a2b027ffda563247
Stepan <stepan151@gmail.com>
2021-11-05 11:22:46 +0300

    vector reverse iter

--- ft_vector/ft_vector_reverse_iterator.hpp
+++ ft_vector/ft_vector_reverse_iterator.hpp
@@ -20,9 +20,123 @@ namespace ft {
 	public:
 		VectorReverseIterator() : m_it(){}
 		explicit VectorReverseIterator(iterator_type it) : m_it(it) {}
+		//template <typename Iter> если надо, то вниз уйдет <Iter>
 		VectorReverseIterator(const VectorReverseIterator<Iterator> &rev_it) : m_it(rev_it.base()) {}
 		~VectorReverseIterator() {}
+
+		iterator_type base() const {return (this->m_it);}
+		// *
+		reference operator*() const {return (*this->m_it);}
+		// +
+		VectorReverseIterator operator+(difference_type n) const {
+			return (ReverseIterator(this->m_it - n));
+		}
+		// ++iterator
+		VectorReverseIterator operator++() {
+			--this->m_it;
+			return (*this);
+		}
+		// iterator++
+		VectorReverseIterator operator++(int) {
+			VectorReverseIterator tmp(*this);
+			--this->m_it;
+			return (tmp);
+		}
+		// +=
+		VectorReverseIterator &operator+=(difference_type n) {
+			this->m_it = this->m_it - n;
+			return (*this);
+		}
+		// -
+		VectorReverseIterator operator-(difference_type n) const {
+			return (VectorReverseIterator(this->m_it + n));
+		}
+		// --iterator
+		VectorReverseIterator operator--() {
+			++this->m_it;
+			return (*this);
+		}
+		// iterator--
+		VectorReverseIterator operator--(int) {
+			VectorReverseIterator tmp(*this);
+			++this->m_it;
+			return (tmp);
+		}
+		// -=
+		VectorReverseIterator &operator-=(difference_type n) {
+			this->m_it = this->m_it + n;
+			return (*this);
+		}
+//		bool operator==(reverse_iterator const &ref) const {
+//			return (this->m_it == ref.m_it);
+//		}
+//		bool operator!=(reverse_iterator const &ref) const {
+//			return (this->m_it != ref.m_it);
+//		}
+		// ->
+		pointer operator->() const {return (&(this->operator*()));}
+		/*	Accesses the element located n positions away from the element
+		 * currently pointed to by the iterator.
+		 * If such an element does not exist, it causes undefined behavior.
+		 * Internally, the function accesses the proper element of its base iterator,
+		 * returning the same as: base()[-n-1].
+		*/
+		reference operator[](difference_type n) const {return (base()[-n-1]);} //???
+
 	};
+	/*
+	 * relational operators (reverse_iterator)
+	 * https://www.cplusplus.com/reference/iterator/reverse_iterator/operators/
+	*/
+	template <class Iterator>
+	bool operator==(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() == rhs.base());
+	}
+	template <class Iterator>
+	bool operator!=(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() != rhs.base());
+	}
+	template <class Iterator>
+	bool operator<(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() < rhs.base());
+	}
+	template <class Iterator>
+	bool operator<=(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() <= rhs.base());
+	}
+	template <class Iterator>
+	bool operator>(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() > rhs.base());
+	}
+	template <class Iterator>
+	bool operator>=(const VectorReverseIterator<Iterator>& lhs, const VectorReverseIterator<Iterator>& rhs) {
+		return (lhs.base() >= rhs.base());
+	}
+	///////////////////////////////////////////////////////////////
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator==(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() == rhs.base());
+	}
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator!=(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() != rhs.base());
+	}
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator<(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() < rhs.base());
+	}
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator<=(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() <= rhs.base());
+	}
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator>(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() > rhs.base());
+	}
+	template <class Iterator_Lhs, class Iterator_Rhs>
+	bool operator>=(const VectorReverseIterator<Iterator_Lhs>& lhs, const VectorReverseIterator<Iterator_Rhs>& rhs) {
+		return (lhs.base() >= rhs.base());
+	}
 }
 
 #endif
