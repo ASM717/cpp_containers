b20adbcf21ca0a1d7f57beefd729effc0dccafa1
ASM717 <stepan151@gmail.com>
2021-12-07 19:58:36 +0300

    bidirectional iter

--- ft_map/ft_bidirectional_iterator.hpp
+++ ft_map/ft_bidirectional_iterator.hpp
@@ -14,13 +14,6 @@ namespace ft {
 	// : public ft::iterator<ft::bidirectional_iterator_tag>
 	class BidirectionalIterator {
 		typedef T value_type;
-		// typedef ft::Node<T>* pointer;
-		// typedef T& reference;
-		// typedef ft::Node<T>* const_pointer;
-		// typedef T& const_reference;
-		// typedef std::bidirectional_iterator_tag iterator_category;
-		// typedef typename ft::iterator_traits<T>::difference_type difference_type;
-
 		typedef typename ft::iterator_traits<T>::size_type size_type;
 		typedef typename ft::iterator_traits<T>::pointer pointer;
 		typedef typename ft::iterator_traits<T>::reference reference;
